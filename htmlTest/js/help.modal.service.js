5171afe675cc7afc819ec39c4ac48ce603741681
yujian <1317545539@qq.com>
2018-03-19 18:51:59 +0800

    修改

--- htmlTest/js/help.modal.service.js
+++ htmlTest/js/help.modal.service.js
@@ -1,14 +1,14 @@
-app.service('helpModalService',[
+app.service('helpModalService', [
     '$rootScope',
     '$ionicModal',
-    function(
+    function (
         $rootScope,
         $ionicModal
-    ){
+    ) {
         var $scope = $rootScope.$new();
         var callbacks = {};
         var helpModalService = {
-            showHelp1: function(param, success, fail){
+            showHelp1: function (param, success, fail) {
                 callbacks = {
                     success: success,
                     fail: fail
@@ -18,27 +18,40 @@ app.service('helpModalService',[
         }
 
         var help1 = {
-            show: function(){
-                $ionicModal.fromTemplateUrl('../templates/help.modal.service.html',{
-                    scope:$scope,
-                    animation:'slide-in-left'
-                }).then(function(modal){
-                    console.log(modal);
+            show: function () {
+                $ionicModal.fromTemplateUrl('../templates/help.modal.service.html', {
+                    scope: $scope,
+                    animation: 'slide-in-left'
+                }).then(function (modal) {
+                    // console.log(modal);
                     $scope.help1 = modal;
                     $scope.help1.show();
-                },function(){
+                }, function () {
 
                 });
             },
-            remove: function(){
-                if($scope.help1){
+            remove: function () {
+                if ($scope.help1) {
                     $scope.help1.remove();
                 }
             },
-            isShown: function(){
-                return $scope.help1&&$scope.help1.isShown();
+            isShown: function () {
+                return $scope.help1 && $scope.help1.isShown();
             }
         }
+
+        $scope.closePage = {
+            help1: function () {
+                help1.remove();
+            }
+        }
+
+        $scope.$on('$locationChangeStart', function (event) {
+            if (help1.isShown()) {
+                help1.remove();
+                event.preventDefault();
+            }
+        })
 
         return helpModalService;
     }
